58530a9957b7772369712a6e0dc3c9c2e397beaa
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-04-13 13:03:20 +0100

    added food pairing list, successfully added food pairing list items to food pairing header with .forEach

--- src/views/display_beer_view.js
+++ src/views/display_beer_view.js
@@ -37,6 +37,27 @@ DisplayBeerView.prototype.render = function (beer) {
   const description = document.createElement('p');
   description.textContent = beer.description;
   this.container.appendChild(description);
+
+  const foodPairingHead = document.createElement('h4');
+  foodPairingHead.classList.add('food-pairing');
+  foodPairingHead.textContent = "Food Pairings";
+  this.container.appendChild(foodPairingHead);
+
+  this.loopThruFoodPairings(beer.food_pairing);
+  // beer.food_pairing.forEach( (foodItem) => {
+  //   foodPairingList.textContent = foodItem;
+  // });
+  // foodPairingHead.appendChild(foodPairingList);
+};
+
+DisplayBeerView.prototype.loopThruFoodPairings = function (foodPairingArray) {
+  foodPairingArray.forEach( (foodItem) => {
+    const foodPairingListItem = document.createElement('li');
+    foodPairingListItem.textContent = foodItem;
+    foodPairingHead = document.querySelector('.food-pairing');
+    foodPairingHead.appendChild(foodPairingListItem);
+    console.log("food pairing:",foodItem);
+  });
 };
 
 module.exports = DisplayBeerView;
