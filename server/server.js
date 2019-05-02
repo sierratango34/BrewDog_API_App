429eacd5fe7911308bb4fdcf940cd48bf390800d
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-05-02 16:36:34 +0100

    sorted routes and gets random beer

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const AllBeer = require("./models/all_beer.js");
+
+const beer = new AllBeer(
+  "https://api.punkapi.com/v2/beers?page=",
+  "&per_page=80"
+);
 
 const publicPath = path.join(__dirname, "../public");
 app.use(express.static(publicPath));
 
-app.use("/", function(req, res) {
+app.use("/api/random", (req, res) => {
+  const randomBeer = beer.getRandomBeer();
+  console.log(randomBeer);
+  res.send(beer.getRandomBeer());
+});
+
+app.use("", function(req, res) {
   res.send();
 });
 
